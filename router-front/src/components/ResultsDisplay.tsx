644f3584df8dccfbdcb679ac31910de94d6c832d
boyrazcan33 <ugurboy94@gmail.com>
2025-07-26 04:33:17 +0300

    result eng

--- router-front/src/components/ResultsDisplay.tsx
+++ router-front/src/components/ResultsDisplay.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLanguage } from '../contexts/LanguageContext';
 import { RouteResponse } from '../types';
 import './ResultsDisplay.scss';
 
@@ -7,9 +8,11 @@ interface ResultsDisplayProps {
 }
 
 const ResultsDisplay: React.FC<ResultsDisplayProps> = ({ result }) => {
+    const { t } = useLanguage();
+
     return (
         <div className="results-display">
-            <h3>Optimizasyon Sonucu</h3>
+            <h3>{t.optimizationResult}</h3>
             <div className="result-content">
                 <pre>{JSON.stringify(result, null, 2)}</pre>
             </div>
